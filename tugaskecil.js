67f764806b3268012d758b4738f52bf68c27ce96
unknown <neet693@gmail.com>
2023-02-11 11:40:09 +0700

    Belajar Array

--- tugaskecil.js
+++ tugaskecil.js
@@ -5,3 +5,8 @@ let tanggal = new Date();
 let namaHari = hariNama[tanggal.getDay()];
 alert(`Hari ini adalah hari ${namaHari} dan saldo anda adalah ${saldo}`);
 console.log(`Hari ini adalah hari ${namaHari} dan saldo anda adalah ${saldo}`);
+
+//Soal No. 3
+const nilaiArray = [4, 6, 8, , 12, 14];
+const nilaiBintang = nilaiArray[0] + nilaiArray[nilaiArray.length - 2] / 2;
+console.log("Nilai yang bernilai bintang: " + nilaiBintang);
